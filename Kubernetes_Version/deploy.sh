a1a5106f8f99ca01f316fdbd9539dc852f5827ac
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-04-23 01:11:38 +0500

    worked more

--- Kubernetes_Version/deploy.sh
+++ Kubernetes_Version/deploy.sh
@@ -8,8 +8,8 @@ kubectl apply -f mongodb-configmap.yaml
 kubectl apply -f mongodb-secret.yaml
 kubectl apply -f mongodb-depl-serv.yaml
 
-kubectl apply -f frontend-depl-serv.yaml
-
 kubectl apply -f timetable-depl-serv.yaml
+
+kubectl apply -f frontend-depl-serv.yaml
 
 kubectl apply -f ingress.yaml
