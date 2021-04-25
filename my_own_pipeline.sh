c1f8e97a1ba4e5fa6f3bec7ece6ef0ff389f0384
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-04-26 01:42:26 +0500

    worked more

--- my_own_pipeline.sh
+++ my_own_pipeline.sh
@@ -7,55 +7,92 @@ gcp_projectId='portfolio-apps-311617'
 gcp_bucket='portfolio_content_app'
 
 echo 'Entering Project Path'
-echo 'installing yaml by npm install'
+echo 'installing yaml'
+echo 'running npm install'
 npm install
 
 echo 'Generating App Folder'
+echo 'running set_baseURL_in_utilities.js $baseURL_for_App'
 node set_baseURL_in_utilities.js $baseURL_for_App
 echo 'baseURL set'
+echo 'running cd App/frontend'
 cd App/frontend
+echo 'running npm install'
 npm install
+echo 'running npm run build'
 npm run build
+echo 'running rm -rf ../backend/build/*'
 rm -rf ../backend/build/*
+echo 'running cp ./build/* ../backend/build/'
 cp ./build/* ../backend/build/
+echo 'running cd ..'
 cd ..
 
 echo 'Generating Containerzied_Version Folder'
+echo 'running node set_baseURL_in_utilities.js $baseURL_for_Containerized_Version'
 node set_baseURL_in_utilities.js $baseURL_for_Containerized_Version
+echo 'running cd App/frontend'
 cd App/frontend
+echo 'running npm run build'
 npm run build
+echo 'running rm -rf ../../Containers_Version/image_sources/frontend_service/build/*'
 rm -rf ../../Containers_Version/image_sources/frontend_service/build/*
+echo 'running cp ./build/* ../../Containers_Version/image_sources/frontend_service/build/'
 cp ./build/* ../../Containers_Version/image_sources/frontend_service/build/
+echo 'running cd ..'
 cd ..
 
 
 echo 'Generating Kubernetes_Version Folder'
+echo 'running node set_baseURL_in_utilities.js $baseURL_for_Kubernetes_Version'
 node set_baseURL_in_utilities.js $baseURL_for_Kubernetes_Version
+echo 'running cd App/frontend'
 cd App/frontend
+echo 'running npm run build'
 npm run build
+echo 'running rm -rf node_modules'
 rm -rf node_modules
+echo 'running rm -rf ../../Kubernetes_Version/container_sources/frontend_source/build/*'
 rm -rf ../../Kubernetes_Version/container_sources/frontend_source/build/*
+echo 'running cp ./build/* ../../Kubernetes_Version/container_sources/frontend_source/build/'
 cp ./build/* ../../Kubernetes_Version/container_sources/frontend_source/build/
+echo 'running cd ../..'
 cd ../..
+echo 'running node set_docker_build_index_for_all_images.js'
 node set_docker_build_index_for_all_images.js # a script that increments docker image name and links in all deployments
+echo 'running node push_all_docker_images_to_docker_registy.js'
 node push_all_docker_images_to_docker_registy.js # a script that pushes all docker images
 
 echo 'Pushing to Github'
+echo 'running git add -A'
 git add -A
+echo 'running git commit -m "worked more"'
 git commit -m "worked more"
+echo 'running git push -u origin main'
 git push -u origin main
 
 echo 'Deploying to App Engine'
+echo 'running node set_baseURL_in_utilities.js ${baseURL_for_App_engine}'
 node set_baseURL_in_utilities.js ${baseURL_for_App_engine}
+echo 'running cd App/frontend'
 cd App/frontend
+echo 'running npm install'
 npm install
+echo 'running npm run build'
 npm run build
+echo 'running rm -rf node_modules'
 rm -rf node_modules
+echo 'running rm -rf ../backend/build/*'
 rm -rf ../backend/build/*
+echo 'running cp ./build/* ../backend/build/'
 cp ./build/* ../backend/build/
+echo 'running cd ../..'
 cd ../..
+echo 'running node generate_dot_env_file_for_cloud_storage.js $gcp_keyFilename $gcp_projectId $gcp_bucket'
 node generate_dot_env_file_for_cloud_storage.js $gcp_keyFilename $gcp_projectId $gcp_bucket
+echo 'running cd App/backend'
 cd App/backend
 # e.g node generate_dot_env_file_for_cloud_storage.js portfolio-apps-311617-9e5ae7843e5d.json portfolio-apps-311617 portfolio_content_app
 
-gcloud app deploy'
+echo 'running gcloud app deploy'
+gcloud app deploy
