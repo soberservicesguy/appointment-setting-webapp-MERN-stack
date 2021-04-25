fb9308a4364da5d8b888ebef2ca06d7dcf3f45ce
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-04-25 21:55:32 +0500

    worked more

--- App/frontend/src/utilities.js
+++ App/frontend/src/utilities.js
@@ -3,7 +3,8 @@ const utils = {
     // baseUrl:'http://localhost:3001', // for development
     // baseUrl:'http://localhost:80', // for docker
     // baseUrl:'http://hello-world.info:80', // for Kubernetes 
-    baseUrl:'https://portfolio-apps-311617.uc.r.appspot.com', // for compute engine 
+    // baseUrl:'https://portfolio-apps-311617.uc.r.appspot.com', // for compute engine 
+	baseUrl: '129012901',
 
 }
 
