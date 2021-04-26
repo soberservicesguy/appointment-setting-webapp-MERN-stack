3e394e8bfccaea27db63f60ba9075f1b3385fc55
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-04-26 20:15:51 +0500

    worked more

--- Kubernetes_Version/container_sources/frontend_source/build_image_and_push.sh
+++ Kubernetes_Version/container_sources/frontend_source/build_image_and_push.sh
@@ -9,6 +9,7 @@
 
 
 
-docker image build -t appointment_frontend_17 .
-docker image tag appointment_frontend_17 soberservicesguy/portfolio-images:appointment_frontend_17
-docker image push soberservicesguy/portfolio-images:appointment_frontend_17
+
+docker image build -t appointment_frontend_18 .
+docker image tag appointment_frontend_18 soberservicesguy/portfolio-images:appointment_frontend_18
+docker image push soberservicesguy/portfolio-images:appointment_frontend_18
